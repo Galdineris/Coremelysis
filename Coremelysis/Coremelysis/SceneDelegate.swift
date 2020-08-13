f7f0f725c2e039ac47153956deefe9f77d387fdb
Rafael Galdino <galdineris@gmail.com>
2020-08-13 20:18:01 -0300

    Add XCode base project
    Motivation:
    
    Set up the base project for the app.
    
    Modification:
    
    Created the XCode project with ViewCode properties in mind. Added empty
    MainViewController class to mock the basic functionality.
    
    Result:
    
    A clean iOS Single View Project in XCode.

---
+++ Coremelysis/Coremelysis/SceneDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  SceneDelegate.swift
+//  Coremelysis
+//
+//  Created by Rafael Galdino on 13/08/20.
+//  Copyright © 2020 Rafael Galdino. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let vc = MainViewController(nibName: nil, bundle: nil)
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = vc
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+
+}
+
