4e5ff363ec095eb1ef8aa5763bb8bf02654f1101
csfar <csf.artur@gmail.com>
2020-08-18 20:40:18 -0300

    Add barebones inference
    Motivation:
    Implement basic sentiment analysis to the main view.
    
    Modifications:
    Added a viewmodel to the main view with a function to perform a
    sentiment analysis on a paragraph. This functions returns a string
    with the results of the analysis using a enum called `Sentiment`.
    
    Result:
    Basis sentiment analysis can now be performed on the main view.

--- Coremelysis/Coremelysis/SceneDelegate.swift
+++ Coremelysis/Coremelysis/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let vc = MainViewController()
+        let vc = MainViewController(viewModel: MainViewModel())
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = vc
