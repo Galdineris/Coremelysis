2e9aab81c774f3340f149d4368bd88ea54fd1942
csfar <csf.artur@gmail.com>
2020-09-28 09:24:44 -0300

    Document CoreDataStack.swift

--- Coremelysis/Coremelysis/Core Data/CoreDataStack.swift
+++ Coremelysis/Coremelysis/Core Data/CoreDataStack.swift
@@ -8,10 +8,15 @@
 
 import CoreData
 
+/// Object responsible for initializing the Core Data stack and provide access to the main context
+/// through a computed variable.
 final class CoreDataStack {
-
+    // - MARK: Properties
+    /// The model's name. It has a default value of `Coremelysis`.
     private let model: String
 
+    /// The container lazily initialized. It sets the main context with the
+    /// `automaticallyMergesChangesFromParent` property as true.
     private lazy var container: NSPersistentContainer = {
         let container = NSPersistentContainer(name: self.model)
         let defaultURL = NSPersistentContainer.defaultDirectoryURL()
@@ -29,18 +34,23 @@ final class CoreDataStack {
         return container
     }()
 
+    /// A computed variable that gives access to the container's `viewContext` property.
+    /// As this context is set to work on the main thread it should be used for UI-related tasks.
     var mainContext: NSManagedObjectContext {
         return container.viewContext
     }
 
-    init(model: String) {
+    // - MARK: Init
+    /// Initializes a new instance of this type.
+    /// - Parameter model: The model's name. It has a default value of `Coremelysis`.
+    init(model: String = "Coremelysis") {
         self.model = model
     }
 
+    /// Saves any changes in the main context. If no changes have been made, nothing will happen.
     func save() {
         if mainContext.hasChanges {
             do {
-
                 try mainContext.save()
             } catch {
                 // - TODO: Handle error
